12a643595ce3e5a47ef6fb4265aa6db5ca2fe6e7
VladislavRomash <romashkanvlad@gmail.com>
2023-10-25 12:34:22 +0300

    Created component of Profile

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import './App.css'
 import {Header} from './components/Header';
 import {Navigation} from './components/Navigation';
+import {Profile} from './components/Profile';
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navigation/>
-            <div className="content">
-                <img
-                    src="https://png.pngtree.com/thumb_back/fh260/background/20200621/pngtree-abstract-modern-neon-frame-background-image_339537.jpg"
-                    alt=""/>
-                <div>ava+description</div>
-                <div>My posts</div>
-                <div> New post</div>
-                <div>post_1</div>
-                <div>post_2</div>
-            </div>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <img
+                src="https://png.pngtree.com/thumb_back/fh260/background/20200621/pngtree-abstract-modern-neon-frame-background-image_339537.jpg"
+                alt=""/>
+            <div>ava+description</div>
+            <div>My posts</div>
+            <div> New post</div>
+            <div>post_1</div>
+            <div>post_2</div>
+        </div>
+    );
+};
