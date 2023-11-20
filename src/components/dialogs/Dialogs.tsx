9e48e93d21b2e5acf4cb42d423fd2e5f3746a202
VladislavRomash <romashkanvlad@gmail.com>
2023-11-20 14:02:42 +0300

    Lesson 21 is completed (Making up the Dialogs page)

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,8 +3,39 @@ import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <>
-            Dialogs
-        </>
+        <div className={style.dialogs}>
+
+            <div className={style.dialogsItems}>
+
+                <div className={`${style.dialog} ${style.active}`}>
+                    Dima
+                </div>
+
+                <div className={style.dialog}>
+                    Ann
+                </div>
+
+                <div className={style.dialog}>
+                    Igor
+                </div>
+
+            </div>
+
+            <div className={style.messagesItems}>
+
+                <div className={style.message}>
+                    Hi
+                </div>
+
+                <div className={style.message}>
+                    How are you?
+                </div>
+
+                <div className={style.message}>
+                    Yo
+                </div>
+
+            </div>
+        </div>
     );
 };
